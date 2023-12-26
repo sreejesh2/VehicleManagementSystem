c273ab17bb9556e8d98b165e01fd289a8df43b63
sreejesh-vs <sreejesh-vs@industryapps.net>
2023-12-26 16:02:44 +0530

    in

--- vehiclemanagement/settings.py
+++ vehiclemanagement/settings.py
@@ -80,13 +80,25 @@ WSGI_APPLICATION = 'vehiclemanagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'your_database_name',
+#         'USER': 'your_database_user',
+#         'PASSWORD': 'your_database_password',
+#         'HOST': 'localhost',  # Set to the PostgreSQL host
+#         'PORT': '5432',       # Set to the PostgreSQL port
+#     }
+# }
+
+DATABASE_URL = "postgres://betlcvai:0e41JYC9QwrLGB02CGkOogTsdyS3FZt3@drona.db.elephantsql.com/betlcvai"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
